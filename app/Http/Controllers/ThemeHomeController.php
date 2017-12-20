27b677e8743d07551cc68da09ea57c3ee0f9b3a0
sitahmetemin <sitahmetemin@hotmail.com>
2017-12-20 04:50:38 +0300

    Sistem Tamamladı.

--- app/Http/Controllers/ThemeHomeController.php
+++ app/Http/Controllers/ThemeHomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\GirisKayit;
 use App\Oteller;
 use App\Iller;
 use Illuminate\Http\Request;
@@ -9,8 +10,9 @@ use Illuminate\Http\Request;
 class ThemeHomeController extends Controller
 {
     public function view() {
+
         return view('theme.home', [
-            'oteller' => Oteller::inRandomOrder()->take(6)->get()
+            'oteller' => Oteller::inRandomOrder()->take(6)->get(),
         ]);
     }
 
--- resources/views/theme/home.blade.php
+++ resources/views/theme/home.blade.php
@@ -114,7 +114,8 @@
                             <div class="text-overlay-meta">
                                 <span>{{ $otel->created_at->diffForHumans() }}</span>
                             </div>
-                            <a href="/otel-detay/{{ $otel->id }}" class="button button-reveal button-border button-light button-small button-rounded uppercase tright noleftmargin topmargin-sm"><span>Detaylar</span><i
+                            <a href="/otel-detay/{{ $otel->id }}"
+                               class="button button-reveal button-border button-light button-small button-rounded uppercase tright noleftmargin topmargin-sm"><span>Detaylar</span><i
                                         class="icon-angle-right"></i></a>
                         </div>
                         <div class="rounded-skill" data-color="#e74c3c" data-trackcolor="rgba(0,0,0,0.1)" data-size="80" data-percent="{{ $otel->yildiz }}00" data-width="{{ $otel->yildiz }}"
@@ -169,152 +170,7 @@
                             <h3>Güçlü Sistem Yaklaşımı</h3>
                         </div>
                     </div>
-
                 </div>
-
-            </div>
-
-            <div class="section topmargin nobottommargin nobottomborder">
-                <div class="container clearfix">
-                    <div class="heading-block center nomargin">
-                        <h3>En Çok Tercih Edilen İller</h3>
-                    </div>
-                </div>
-            </div>
-
-            <div id="portfolio" class="portfolio portfolio-nomargin grid-container portfolio-notitle portfolio-full grid-container clearfix">
-
-                <article class="portfolio-item pf-media pf-icons">
-                    <div class="portfolio-image">
-                        <a href="portfolio-single.html">
-                            <img src="/assets/theme/images/portfolio/4/1.jpg" alt="Open Imagination">
-                        </a>
-                        <div class="portfolio-overlay">
-                            <a href="/assets/theme/images/portfolio/full/1.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-                            <a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
-                        </div>
-                    </div>
-                    <div class="portfolio-desc">
-                        <h3><a href="portfolio-single.html">Open Imagination</a></h3>
-                        <span><a href="#">Media</a>, <a href="#">Icons</a></span>
-                    </div>
-                </article>
-
-                <article class="portfolio-item pf-illustrations">
-                    <div class="portfolio-image">
-                        <a href="portfolio-single.html">
-                            <img src="/assets/theme/images/portfolio/4/2.jpg" alt="Locked Steel Gate">
-                        </a>
-                        <div class="portfolio-overlay">
-                            <a href="/assets/theme/images/portfolio/full/2.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-                            <a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
-                        </div>
-                    </div>
-                    <div class="portfolio-desc">
-                        <h3><a href="portfolio-single.html">Locked Steel Gate</a></h3>
-                        <span><a href="#">Illustrations</a></span>
-                    </div>
-                </article>
-
-                <article class="portfolio-item pf-graphics pf-uielements">
-                    <div class="portfolio-image">
-                        <a href="#">
-                            <img src="/assets/theme/images/portfolio/4/3.jpg" alt="Mac Sunglasses">
-                        </a>
-                        <div class="portfolio-overlay">
-                            <a href="http://vimeo.com/89396394" class="left-icon" data-lightbox="iframe"><i class="icon-line-play"></i></a>
-                            <a href="portfolio-single-video.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
-                        </div>
-                    </div>
-                    <div class="portfolio-desc">
-                        <h3><a href="portfolio-single-video.html">Mac Sunglasses</a></h3>
-                        <span><a href="#">Graphics</a>, <a href="#">UI Elements</a></span>
-                    </div>
-                </article>
-
-                <article class="portfolio-item pf-icons pf-illustrations">
-                    <div class="portfolio-image">
-                        <a href="portfolio-single.html">
-                            <img src="/assets/theme/images/portfolio/4/4.jpg" alt="Open Imagination">
-                        </a>
-                        <div class="portfolio-overlay" data-lightbox="gallery">
-                            <a href="/assets/theme/images/portfolio/full/4.jpg" class="left-icon" data-lightbox="gallery-item"><i class="icon-line-stack-2"></i></a>
-                            <a href="/assets/theme/images/portfolio/full/4-1.jpg" class="hidden" data-lightbox="gallery-item"></a>
-                            <a href="portfolio-single-gallery.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
-                        </div>
-                    </div>
-                    <div class="portfolio-desc">
-                        <h3><a href="portfolio-single-gallery.html">Morning Dew</a></h3>
-                        <span><a href="#"><a href="#">Icons</a>, <a href="#">Illustrations</a></span>
-                    </div>
-                </article>
-
-                <article class="portfolio-item pf-uielements pf-media">
-                    <div class="portfolio-image">
-                        <a href="portfolio-single.html">
-                            <img src="/assets/theme/images/portfolio/4/5.jpg" alt="Console Activity">
-                        </a>
-                        <div class="portfolio-overlay">
-                            <a href="/assets/theme/images/portfolio/full/5.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-                            <a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
-                        </div>
-                    </div>
-                    <div class="portfolio-desc">
-                        <h3><a href="portfolio-single.html">Console Activity</a></h3>
-                        <span><a href="#">UI Elements</a>, <a href="#">Media</a></span>
-                    </div>
-                </article>
-
-                <article class="portfolio-item pf-graphics pf-illustrations">
-                    <div class="portfolio-image">
-                        <a href="portfolio-single.html">
-                            <img src="/assets/theme/images/portfolio/4/6.jpg" alt="Open Imagination">
-                        </a>
-                        <div class="portfolio-overlay" data-lightbox="gallery">
-                            <a href="/assets/theme/images/portfolio/full/6.jpg" class="left-icon" data-lightbox="gallery-item"><i class="icon-line-stack-2"></i></a>
-                            <a href="/assets/theme/images/portfolio/full/6-1.jpg" class="hidden" data-lightbox="gallery-item"></a>
-                            <a href="/assets/theme/images/portfolio/full/6-2.jpg" class="hidden" data-lightbox="gallery-item"></a>
-                            <a href="/assets/theme/images/portfolio/full/6-3.jpg" class="hidden" data-lightbox="gallery-item"></a>
-                            <a href="portfolio-single-gallery.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
-                        </div>
-                    </div>
-                    <div class="portfolio-desc">
-                        <h3><a href="portfolio-single-gallery.html">Shake It!</a></h3>
-                        <span><a href="#">Illustrations</a>, <a href="#">Graphics</a></span>
-                    </div>
-                </article>
-
-                <article class="portfolio-item pf-uielements pf-icons">
-                    <div class="portfolio-image">
-                        <a href="portfolio-single-video.html">
-                            <img src="/assets/theme/images/portfolio/4/7.jpg" alt="Backpack Contents">
-                        </a>
-                        <div class="portfolio-overlay">
-                            <a href="http://www.youtube.com/watch?v=kuceVNBTJio" class="left-icon" data-lightbox="iframe"><i class="icon-line-play"></i></a>
-                            <a href="portfolio-single-video.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
-                        </div>
-                    </div>
-                    <div class="portfolio-desc">
-                        <h3><a href="portfolio-single-video.html">Backpack Contents</a></h3>
-                        <span><a href="#">UI Elements</a>, <a href="#">Icons</a></span>
-                    </div>
-                </article>
-
-                <article class="portfolio-item pf-graphics">
-                    <div class="portfolio-image">
-                        <a href="portfolio-single.html">
-                            <img src="/assets/theme/images/portfolio/4/8.jpg" alt="Sunset Bulb Glow">
-                        </a>
-                        <div class="portfolio-overlay">
-                            <a href="/assets/theme/images/portfolio/full/8.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-                            <a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
-                        </div>
-                    </div>
-                    <div class="portfolio-desc">
-                        <h3><a href="portfolio-single.html">Sunset Bulb Glow</a></h3>
-                        <span><a href="#">Graphics</a></span>
-                    </div>
-                </article>
             </div>
         </div>
     </section><!-- #content end -->
